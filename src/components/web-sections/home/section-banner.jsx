030006e9b56691d5782eb7b1ea2fe758179dca8b
Mauro <maurolobo.ml@gmail.com>
2025-06-30 13:43:36 -0300

    fix detail

--- src/components/web-sections/home/section-banner.jsx
+++ src/components/web-sections/home/section-banner.jsx
@@ -268,7 +268,7 @@ const SectionBanner = () => {
             pointerEvents: 'auto'
           }}
         >
-          <div className="w-[300px] min-d:w-[530px] min-bannerT:w-[720px]  min-d:mt-24 min-note:mt-38  relative overflow-hidden ">
+          <div className="w-[300px] min-d:w-[530px] min-bannerT:w-[630px]  min-d:mt-24 min-note:mt-38  relative overflow-hidden ">
             <div className="relative z-10 text-[var(--color-three)] flex flex-col min-d:justify-end min-d:items-end min-bannerT:justify-start min-bannerT:items-start">
               <h1 className="text-(length:--text-title-huge) font-bold  leading-none ">SQM</h1>
               <h1 className="text-(length:--text-title-huge) font-bold  leading-none ">GÜEMES</h1>

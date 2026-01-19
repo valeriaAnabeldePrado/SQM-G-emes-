99dc2b49f212c66cceac24d4328f6956eb799b8f
Anabel de Prado <anabeltayu@gmail.com>
2026-01-19 09:20:16 -0300

    fix detalil card departament details

--- src/components/web-sections/apartments/ApartmentDetail.jsx
+++ src/components/web-sections/apartments/ApartmentDetail.jsx
@@ -1233,7 +1233,7 @@ const ApartmentDetail = () => {
     return (
       <>
         <div className="custom-container mt-20 px-4">
-          <h2 className="text-[var(--color-three)] text-2xl mb-4">Departamento no encontrado</h2>
+          <h2 className="text-threetext-2xl mb-4">Departamento no encontrado</h2>
           <Button onClick={() => navigate('/apartments')} className="mt-4">
             <MdOutlineArrowBack size="1.2em" className="mr-2" />
             Volver a apartamentos
@@ -1247,20 +1247,20 @@ const ApartmentDetail = () => {
   return (
     <>
       <div className="custom-container  mt-14   px-4 ">
-        <div className="flex min-md:items-center items-start min-md:flex-row flex-col gap-6 mb-12">
+        <div className="flex md:items-center items-start md:flex-row flex-col gap-6 mb-12">
           <Button
             onClick={() => navigate('/apartments')}
-            className="z-50 flex items-center cursor-pointer gap-2 bg-[var(--color-three)] hover:bg-opacity-90 text-white px-6 py-3 rounded-full"
+            className="z-50 flex items-center cursor-pointer gap-2 bg-three hover:bg-opacity-90 text-white px-6 py-3 rounded-full"
           >
             <MdOutlineArrowBack size="1.2em" />
             Volver
           </Button>
-          <h1 className="text-xl font-bold text-[var(--color-three)]">
+          <h1 className="text-xl font-bold text-three">
             {apartment.title.replace(' - Piso 1', '')}
           </h1>
         </div>
 
-        <div className="grid grid-cols-1 min-[768px]:grid-cols-2 min-[768px]:grid-rows-4 gap-6 min-[768px]:h-[75vh]">
+        <div className="grid grid-cols-1 min-[768px]:grid-cols-2 min-[768px]:grid-rows-[1.1fr_1fr_1fr_1fr] gap-6 min-[768px]:h-[75vh]">
           <div className="min-[768px]:row-span-4 min-[768px]:col-span-1">
             <div className="bg-white rounded-2xl p-4 shadow-lg h-full overflow-hidden">
               {apartment.floorPlan !== '' ? (
@@ -1302,9 +1302,9 @@ const ApartmentDetail = () => {
             </div>
           </div>
 
-          <div className="min-[768px]:row-span-1 min-[768px]:col-span-1">
+          <div className="min-[768px]:row-span-1 min-[768px]:col-span-1 ">
             <Card hasGradient className="flex flex-col h-full justify-center">
-              <div className="flex w-full p-4">
+              <div className="flex w-full ">
                 <div className="grid grid-cols-1 gap-2 w-full">
                   {apartment.superficiePropia && (
                     <div className="flex justify-between items-end border-b border-[var(--color-three)]/10 pb-1">
